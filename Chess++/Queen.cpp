4da4f811a03ca10b56c903e9ec8b0445bc4a8bf5
LikeTheBoss <brendan@bosspc.com>
2018-10-26 15:36:52 -0400

    Unit Tested and Implement Queen's calculateLegalMoves basic candidate move functionality #8 #9

--- Chess++/Queen.cpp
+++ Chess++/Queen.cpp
@@ -13,5 +13,27 @@ Queen::~Queen()
 
 std::set<Location> Queen::calculateLegalMoves(Board board)
 {
-	return std::set<Location>();
+	std::set<Location> moves;
+
+	// Rook Movement
+	for (int i = 0; i < COLUMNS; i++)
+	{
+		if (i != location.x)
+		{
+			moves.insert({ i, location.y });
+		}
+		if (i != location.y)
+		{
+			moves.insert({ location.x, i });
+		}
+	}
+
+	// Bishop Movement
+	for (int i = 1; i < COLUMNS; i++)
+	{
+		moves.insert({ ((location.x + i) % COLUMNS + COLUMNS) % COLUMNS, ((location.y + i) % ROWS + ROWS) % ROWS });
+		moves.insert({ ((location.x + i) % COLUMNS + COLUMNS) % COLUMNS, ((location.y - i) % ROWS + ROWS) % ROWS });
+	}
+
+	return moves;
 }
--- Chess++Tests/QueenTests.cpp
+++ Chess++Tests/QueenTests.cpp
@@ -1,20 +1,20 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include "../Chess++/Bishop.h"
+#include "../Chess++/Queen.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace ChessTests
 {
-	TEST_CLASS(BishopTests) {
+	TEST_CLASS(QueenTests) {
 		TEST_METHOD(TestCalculateLegalMoves)
 		{
-			Bishop bishop = Bishop(3, 4, WHITE);
+			Queen bishop = Queen(3, 4, WHITE);
 			const auto board = Board();
 
 			auto moves = bishop.calculateLegalMoves(board);
 
-			Assert::AreEqual(13, static_cast<int>(moves.size()));
+			Assert::AreEqual(27, static_cast<int>(moves.size()));
 			Assert::IsTrue(moves.count({ 0, 1 }));
 			Assert::IsTrue(moves.count({ 0, 7 }));
 			Assert::IsTrue(moves.count({ 1, 2 }));
@@ -28,6 +28,21 @@ namespace ChessTests
 			Assert::IsTrue(moves.count({ 6, 7 }));
 			Assert::IsTrue(moves.count({ 6, 1 }));
 			Assert::IsTrue(moves.count({ 7, 0 }));
+			Assert::IsTrue(moves.count({ 0, 4 }));
+			Assert::IsTrue(moves.count({ 1, 4 }));
+			Assert::IsTrue(moves.count({ 2, 4 }));
+			Assert::IsTrue(moves.count({ 0, 4 }));
+			Assert::IsTrue(moves.count({ 4, 4 }));
+			Assert::IsTrue(moves.count({ 5, 4 }));
+			Assert::IsTrue(moves.count({ 6, 4 }));
+			Assert::IsTrue(moves.count({ 7, 4 }));
+			Assert::IsTrue(moves.count({ 3, 0 }));
+			Assert::IsTrue(moves.count({ 3, 1 }));
+			Assert::IsTrue(moves.count({ 3, 2 }));
+			Assert::IsTrue(moves.count({ 3, 3 }));
+			Assert::IsTrue(moves.count({ 3, 5 }));
+			Assert::IsTrue(moves.count({ 3, 6 }));
+			Assert::IsTrue(moves.count({ 3, 7 }));
 		}
 	};
 }
